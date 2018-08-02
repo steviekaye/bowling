f8622e391a02f071e070af7516726f7105878751
steviekaye <stevie.kaye@gmail.com>
2018-08-03 11:19:06 +1200

    Indent code and use Rufo auto-formatter

--- bowling.rb
+++ bowling.rb
@@ -1,28 +1,31 @@
-
 def turn
-first = 1+rand(10)
-second = 0
-if first == 10
-  print "X  |"
+  first = 1 + rand(10)
+  second = 0
+
+  if first == 10
+    print "X  |"
   else
-  second = 1+rand(10-first)
+    second = 1 + rand(10 - first)
+
     if first + second == 10
-    print "#{first} /|"
+      print "#{first} /|"
     else
-    print "#{first} #{second}|"
+      print "#{first} #{second}|"
     end
   end
-$totalpoints = $totalpoints + first + second
+  $totalpoints = $totalpoints + first + second
 end
 
 numturns = 10
 $totalpoints = 0
 puts "_" * 41
 print "|"
+
 until numturns == 0
-turn
-numturns = numturns - 1
+  turn
+  numturns = numturns - 1
 end
+
 print "\n"
 puts "_" * 41
 puts "Total score is #{$totalpoints}"
