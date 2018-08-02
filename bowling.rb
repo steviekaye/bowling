47ac6f9f0531ba526ad24b72bf56aea65687e6d2
steviekaye <stevie.kaye@gmail.com>
2018-08-03 11:09:55 +1200

    added wonky box to turn scores

--- bowling.rb
+++ bowling.rb
@@ -3,13 +3,13 @@ def turn
 first = 1+rand(10)
 second = 0
 if first == 10
-  puts "X"
+  print "X  |"
   else
   second = 1+rand(10-first)
     if first + second == 10
-    puts "#{first} /"
+    print "#{first} /|"
     else
-    puts "#{first} #{second}"
+    print "#{first} #{second}|"
     end
   end
 $totalpoints = $totalpoints + first + second
@@ -17,8 +17,12 @@ end
 
 numturns = 10
 $totalpoints = 0
+puts "_" * 41
+print "|"
 until numturns == 0
 turn
 numturns = numturns - 1
 end
+print "\n"
+puts "_" * 41
 puts "Total score is #{$totalpoints}"
